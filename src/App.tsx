eaf9eb89c553e174b2a075bce831bcfbcb381a9e
bspang <bspangler21@alumni.uwosh.edu>
2026-02-22 16:46:32 -0600

    style: format code and improve consistency in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { makeStyles, tokens } from '@fluentui/react-components';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { makeStyles, tokens } from "@fluentui/react-components";
 
 const useStyles = makeStyles({
 	pageContainer: {
 		display: "flex",
+		width: "100%",
 		height: "100%",
-    margin: 0,
+		margin: 0,
 	},
 	contentContainer: {
 		display: "flex",
@@ -25,24 +26,22 @@ const useStyles = makeStyles({
 		alignItems: "center",
 		backgroundColor: tokens.colorBrandBackground,
 		color: "#FFFFFF",
-		// padding: "8px 16px",
 		minHeight: "50px",
-		// minWidth: "100vw",
 		boxSizing: "border-box",
 	},
 });
 
 function App() {
-  const styles = useStyles();
+	const styles = useStyles();
 
-  return (
-			<div className={styles.pageContainer}>
-				<div className={styles.contentContainer}>
-					<div className={styles.header}></div>
-					<p>Click on the Vite and React logos to learn more</p>
-				</div>
+	return (
+		<div className={styles.pageContainer}>
+			<div className={styles.contentContainer}>
+				<div className={styles.header}></div>
+				<p>Click on the Vite and React logos to learn more</p>
 			</div>
+		</div>
 	);
 }
 
-export default App
+export default App;
