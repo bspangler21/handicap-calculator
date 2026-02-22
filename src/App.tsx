ba432353e8c67a5154f654c79c0eb17533d5e34f
bspang <bspangler21@alumni.uwosh.edu>
2026-02-22 16:56:37 -0600

    feat: enhance layout structure by adding header and footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { makeStyles, tokens } from "@fluentui/react-components";
 
 const useStyles = makeStyles({
 	pageContainer: {
 		display: "flex",
+    flexDirection: "column",
 		width: "100%",
 		height: "100%",
 		margin: 0,
@@ -27,6 +25,19 @@ const useStyles = makeStyles({
 		backgroundColor: tokens.colorBrandBackground,
 		color: "#FFFFFF",
 		minHeight: "50px",
+		width: "100%",
+		boxSizing: "border-box",
+	},
+	footer: {
+		display: "flex",
+		flexDirection: "row",
+		flexWrap: "wrap",
+		justifyContent: "space-between",
+		alignItems: "center",
+		backgroundColor: tokens.colorBrandBackground,
+		color: "#FFFFFF",
+		minHeight: "50px",
+		width: "100%",
 		boxSizing: "border-box",
 	},
 });
@@ -36,10 +47,11 @@ function App() {
 
 	return (
 		<div className={styles.pageContainer}>
+			<div className={styles.header}></div>
 			<div className={styles.contentContainer}>
-				<div className={styles.header}></div>
 				<p>Click on the Vite and React logos to learn more</p>
 			</div>
+			<div className={styles.footer}></div>
 		</div>
 	);
 }
